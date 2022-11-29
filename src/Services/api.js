27ef364069cb56784212dbea71689010b7232570
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-11-29 07:57:03 +0100

    fix title and export api

--- src/Services/api.js
+++ src/Services/api.js
@@ -6,8 +6,10 @@ export const fetchImageWithQuery = async (query, page) => {
 	const response = await axios.get(
 		`https://pixabay.com/api/?key=${APIKEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`
 	);
-	console.log(response);
+
 	return { images: response.data.hits, total: response.data.totalHits };
 };
 
-export default { fetchImageWithQuery };
+const api = { fetchImageWithQuery };
+
+export default api;
