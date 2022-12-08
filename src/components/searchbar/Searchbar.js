43a28b067fe4633daeee251ee36b3767ea6cb686
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-12-08 08:33:57 +0100

    lowercase fetch

---
+++ src/components/searchbar/Searchbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import css from "./Searchbar.module.css";
+import PropTypes from "prop-types";
+
+class Searchbar extends Component {
+	handleSumbit = (e) => {
+		e.preventDefault();
+		const form = e.currentTarget;
+		const query = form.elements.query.value;
+		this.props.onSubmit({ query });
+	};
+
+	render() {
+		return (
+			<header className={css["searchbar"]}>
+				<form className={css["form"]} onSubmit={this.handleSumbit}>
+					<button type="submit" className={css["button"]}>
+						<span className={css["button-label"]}>Search</span>
+					</button>
+
+					<input
+						name="query"
+						className={css["input"]}
+						type="text"
+						autoComplete="off"
+						autoFocus
+						placeholder="Search images and photos"
+					/>
+				</form>
+			</header>
+		);
+	}
+}
+
+Searchbar.propTypes = {
+	onSubmit: PropTypes.func.isRequired,
+};
+
+export default Searchbar;
